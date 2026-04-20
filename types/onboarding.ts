90beea02e4cd6c27c12548279121e043afacae57
1047627-LWSD <1047627@lwsd.org>
2026-04-20 13:21:27 -0700

    Add files via upload

---
+++ types/onboarding.ts
@@ -0,0 +1,9 @@
+export type OnboardingData = {
+  serviceCategory: string;
+  service: string;
+  niche: string;
+  result: string;
+  price: number;
+  monthlyGoal: number;
+  offerStatement: string;
+};
